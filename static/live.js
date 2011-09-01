9e24b2465da53f1d9b4544f12132f522b716e60c
Xion <karol.kuczmarski@gmail.com>
2011-09-01 23:27:51 +0200

    Inserting URLs to gramamtical texts into browser history

--- static/live.js
+++ static/live.js
@@ -3,15 +3,25 @@
 var ajaxRequest = null;
 
 $(document).ready(function() {
+  
   $("#text").keyup(function() {
 
     if (ajaxRequest) ajaxRequest.abort();
 
-    ajaxRequest = $.getJSON("", { text: $("#text").val(), json: true }, function(json) {
+    var jboText = $("#text").val(),
+    ajaxRequest = $.getJSON("", { text: jboText, json: true }, function(json) {
       $("#boxes").html(json.html);
+      
       if (json.grammatical) {
         $("#text").removeClass("ungrammatical");
         $("#text").addClass("grammatical");
+
+        if (typeof(window.history) !== "undefined") {
+          var link = $("<a/>")[0];
+          link.href = window.location.href;
+          link.search = "?text=" + escape(jboText);
+          history.pushState(null, null, link.href);
+        }
       } else {
         $("#text").addClass("ungrammatical");
         $("#text").removeClass("grammatical");
